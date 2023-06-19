4b7aa0a329a06ea6cf63db1d209da664f6d4da1c
hamitmizrak <hamitmizrak@gmail.com>
2023-06-19 21:24:06 +0300

    back to top

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 // Dark Mode 
 let darkModu=()=>{
     //alert("Dark modu");
-    window.document.classList.toggle("dark_mode");
+    document.body.classList.toggle("dark_mode");
 }
 
 // input search
