2cf17add6bf993d7a4bf280bcdbb1a9b5766b09f
hamitmizrak <hamitmizrak@gmail.com>
2023-06-19 20:56:46 +0300

    Search, Dark mode, Header, Footer

--- template.js
+++ template.js
@@ -1,2 +1,25 @@
-// alert("Deneme")
-// window.alert("Deneme")
+// Dark Mode 
+let darkModu=()=>{
+    //alert("Dark modu");
+    window.document.classList.toggle("dark_mode");
+}
+
+// input search
+$(document).ready(function(){
+const searchApi=["Adana","Balikesir","Çorum","Denizli","Diyarbakır","Edirne","Malatya","Van"];
+$("#tags").autocomplete({
+    source:searchApi
+})
+}); //end 
+
+
+// Footer
+let newDate=()=>{
+    const date=new Date().getFullYear();
+// JS DOM
+//document.getElementById("date_id").innerHTML=new Date().getFullYear();
+
+// Jquery DOM
+$("#date_id").html(date);
+}
+newDate()
