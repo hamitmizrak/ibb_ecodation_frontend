f1e6b9394d27346e76ba7b8be4bf80a66d6690fd
hamitmizrak <hamitmizrak@gmail.com>
2023-07-04 19:44:14 +0300

    whoisting, variable, console, alert, window

--- js/tutorials.js
+++ js/tutorials.js
@@ -1,13 +1,60 @@
+// JS 1995 yılında Brandan Eich tarafından yazılıyor.
+// Cross platform
+// OOP uygun.
+
 //single Comment
 /*
 multiple Comment
 */
 
-console.log("loglama");
-console.info("info");
-console.warn("warn");
-console.error("error");
+// console.log("loglama");
+// console.info("info");
+// console.warn("warn");
+// console.error("error");
 
-alert("pop-up bilgilendirme-1");
-window.alert("pop-up bilgilendirme-2");
-window.document.writeln("ekranda yazı");
+// alert("pop-up bilgilendirme-1");
+// window.alert("pop-up bilgilendirme-2");
+// window.document.writeln("ekranda yazı");
+
+// Variable
+var result=4423;
+console.log(typeof result+" => "+result);
+
+var result=44.23;
+console.log(typeof result+" => "+result);
+
+var result="Js öğreniyorum";
+console.log(typeof result+" => "+result);
+
+var result=true;
+console.log(typeof result+" => "+result);
+
+// Naming Convensition
+var deneme="JS Data";
+console.log(deneme);
+
+var deneme44="JS Data";
+console.log(deneme44);
+
+var $deneme44="JS Data";
+console.log($deneme44);
+
+var _$deneme44="JS Data";
+console.log(_$deneme44);
+
+var denemeVerisi="JS Data";
+console.log(denemeVerisi);
+
+//var &~44$deneme44="JS Data";
+//console.log(44$deneme44);
+
+// interpreter, compiler 
+// senkron, asenkron ?
+
+// whoisting
+result2=56;
+var result2;
+
+///////////////////////////////////////////////////$deneme44
+// Operators
+
