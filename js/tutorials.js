7d6a802a1411d1d84242c6f895fce6966e134682
hamitmizrak <hamitmizrak@gmail.com>
2023-07-04 19:27:45 +0300

    javascript

---
+++ js/tutorials.js
@@ -0,0 +1,13 @@
+//single Comment
+/*
+multiple Comment
+*/
+
+console.log("loglama");
+console.info("info");
+console.warn("warn");
+console.error("error");
+
+alert("pop-up bilgilendirme-1");
+window.alert("pop-up bilgilendirme-2");
+window.document.writeln("ekranda yazı");
