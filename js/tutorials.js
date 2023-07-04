8232d458883a6313efa17d358847c0c7bc6f25e3
hamitmizrak <hamitmizrak@gmail.com>
2023-07-04 20:21:20 +0300

    Number, cast

--- js/tutorials.js
+++ js/tutorials.js
@@ -99,19 +99,79 @@ multiple Comment
 
 ///////////////////////////////////////////////////
 // Math
-console.log(Math.PI);
-console.log(Math.E);
-console.log(Math.min(4,1,99,555,25,-100));
-console.log(Math.max(4,1,99,555,25,-100));
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.min(4,1,99,555,25,-100));
+// console.log(Math.max(4,1,99,555,25,-100));
 
-console.log(Math.sqrt(16));
-console.log(Math.abs(-25));
-console.log(Math.pow(2,5));
+// console.log(Math.sqrt(16));
+// console.log(Math.abs(-25));
+// console.log(Math.pow(2,5));
 
-console.log(Math.floor(3.9));
-console.log(Math.ceil(3.1));
-console.log(Math.round(6.4));
-console.log(Math.round(6.5));
+// console.log(Math.floor(3.9));
+// console.log(Math.ceil(3.1));
+// console.log(Math.round(6.4));
+// console.log(Math.round(6.5));
 
-console.log(Math.sin(45));
-console.log( Math.round(Math.random()*9+1) );
+// console.log(Math.sin(45));
+// console.log( Math.round(Math.random()*9+1));
+
+//Örnek-4: Aşağıdaki örnekleri math ile çözelim ?
+// -5.9 sayıyının aşağıdaki işlemleri yaptıralım ?
+// 1-mutlak değeri alsın 5.9
+// 2-yuvarlama yapsın  6.0
+// 3-karesini alsın 36.00
+// 4-karekök alsın 6.0
+// 5-yuvarlama yapsın 6.0
+// 6-)çıkan sonucu 5 bölsün 6/5=1
+// 7-) iki sayı arasından karşılaştırma yapsın en küçüğünü alsın ve 1 ve 5
+// 8-) küçük sayı eğer tekse 3 eklesin çiftse 5 eklesin 
+
+// Örnek-5 : kullanıcı tarafından girilen bir sayıyı negatif mi pozitif mi olduğu ekran yazdıran algoritma ?
+//NOT: cast kullalım ?
+
+// Örnek-6 : kullanıcı tarafından password ve repassword alalım sonrasında bu iki değeri karşılaştırma yapalım
+//eğer aynı girilirse aynı veri yoksa birbirine uymadı yazan algoritma yapalım
+
+///////////////////////////////////////////////////
+// Number 
+var sayi=15000;
+console.log(sayi);
+
+var sayi2=1.5E+4;
+console.log(sayi2);
+
+// binary,octal,decimal, hexadecimal
+
+// binary : 0-1
+var binary=0b00011;
+console.log(binary);
+
+// octal : 0-1-2-3-4-5-6-7
+var octal=0o7612;
+console.log(octal);
+
+// decimal : 0-1-2-3-4-5-6-7-8-9
+var decimal=123456;
+console.log(decimal);
+
+// hexdecimal : 0-1-2-3-4-5-6-7-8-9-A-B-C-D-E-F
+var hexadecimal=0xffa;
+console.log(hexadecimal);
+
+// Number() parseInt() nedir aralarındaki farklar ?
+// String() data.toString nedir aralarındaki farklar ?
+// Cast
+// database, port , api (string)
+console.log(Number("10")+10);
+console.log(parseInt("10")+10);
+
+console.log(String(30)+10);
+var number=30;
+console.log(number.toString()+10);
+
+///////////////////////////////////////////////////
+// var, let, const
+// string
+
+
