2ab36d1fa1baca589b96f21a6f331ab279b1b72d
hamitmizrak <hamitmizrak@gmail.com>
2023-07-04 20:06:18 +0300

    Math, undefined,  isNaN, Infinity

--- js/tutorials.js
+++ js/tutorials.js
@@ -17,33 +17,33 @@ multiple Comment
 // window.document.writeln("ekranda yazı");
 
 // Variable
-var result=4423;
-console.log(typeof result+" => "+result);
+// var result=4423;
+// console.log(typeof result+" => "+result);
 
-var result=44.23;
-console.log(typeof result+" => "+result);
+// var result=44.23;
+// console.log(typeof result+" => "+result);
 
-var result="Js öğreniyorum";
-console.log(typeof result+" => "+result);
+// var result="Js öğreniyorum";
+// console.log(typeof result+" => "+result);
 
-var result=true;
-console.log(typeof result+" => "+result);
+// var result=true;
+// console.log(typeof result+" => "+result);
 
 // Naming Convensition
-var deneme="JS Data";
-console.log(deneme);
+// var deneme="JS Data";
+// console.log(deneme);
 
-var deneme44="JS Data";
-console.log(deneme44);
+// var deneme44="JS Data";
+// console.log(deneme44);
 
-var $deneme44="JS Data";
-console.log($deneme44);
+// var $deneme44="JS Data";
+// console.log($deneme44);
 
-var _$deneme44="JS Data";
-console.log(_$deneme44);
+// var _$deneme44="JS Data";
+// console.log(_$deneme44);
 
-var denemeVerisi="JS Data";
-console.log(denemeVerisi);
+// var denemeVerisi="JS Data";
+// console.log(denemeVerisi);
 
 //var &~44$deneme44="JS Data";
 //console.log(44$deneme44);
@@ -52,9 +52,66 @@ console.log(denemeVerisi);
 // senkron, asenkron ?
 
 // whoisting
-result2=56;
-var result2;
+// result2=56;
+// var result2;
 
-///////////////////////////////////////////////////$deneme44
+///////////////////////////////////////////////////
 // Operators
+// + - * ** / %
+// ! DEĞİL &&=VE ||=VEYA
+// x++
+// x--
+// ++x
+// --x
 
+// =   ATAMA
+// ==  EŞİT  (TÜRÜNE BAKMA)
+// === EŞİT  (TÜRÜNE BAK)
+
+// var number1=40;
+// var number2=5;
+// console.log(number1+number2);
+// console.log(number1-number2);
+// console.log(number1*number2);
+// console.log(number1/number2);
+// console.log(number1%number2);
+
+// cast
+// Number
+// String()
+// var userData=Number(prompt("Lütfen sayı giriniz"));
+// console.log(userData+45);
+
+// undefined
+// var result4;
+// console.log(result4);
+
+// // NaN: Not A Number
+// var result5="asd"/4;
+// console.log(result5);
+
+// // isNaN
+// var result6=4;
+// console.log(isNaN(result6)); //false: sayıdır
+
+// // Infinity
+// console.log(4/0);
+
+///////////////////////////////////////////////////
+// Math
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.min(4,1,99,555,25,-100));
+console.log(Math.max(4,1,99,555,25,-100));
+
+console.log(Math.sqrt(16));
+console.log(Math.abs(-25));
+console.log(Math.pow(2,5));
+
+console.log(Math.floor(3.9));
+console.log(Math.ceil(3.1));
+console.log(Math.round(6.4));
+console.log(Math.round(6.5));
+
+console.log(Math.sin(45));
+console.log( Math.round(Math.random()*9+1) );
