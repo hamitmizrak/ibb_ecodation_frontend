aa4673edafa1a9c08198b22b01f225cb0450019b
hamitmizrak <hamitmizrak@gmail.com>
2023-07-06 20:39:08 +0300

    break, return, continue

--- js/tutorials.js
+++ js/tutorials.js
@@ -452,6 +452,27 @@ const debugData = () => {
 }
 debugData()
 
+////////////////////////////////////////////////////////
+// break: döngünün çalışmasına izin verme
+// return: metotun çalışmasına izin verme
+// continue: sadece bir seferlik o şartta çalışma
+
+// SORU 1<=userData<=50 
+// 1 ile Kullanıcı tarafından alınan bitiş sayısına kadar toplama yapan Algoritma örneği 
+// Örnek: 1<=user 1+2+3+...userdatası
+// Eğer Bu sayılardan 7 sayıyı varsa bunu toplayama dahil etmesin (continue)
+// Bu sayının en fazla 50'e kadar toplama yapabilir (break)
+// bu sayılardan çift olanların kaç tane, sayıların kendisi ve toplamları nedir ?
+// bu sayılardan tek olanların kaç tane, sayıların kendisi ve toplamları nedir ? 
+// secret-Key kullanıcı eğer 44 girerse hiç bir işlem yapmadan sistemden çıkış sağlansın onunda haricinde sürekli işlem yapsın
+// Dikkat: Bu algoritmayı Arrow Function ile yapalım.
+// (Clean code kuralları çercevesinde ) algoritma yapan JS program?
+
+
+
+
+
+
 
 ////////////////////////////////////////////////////////
 // return break continue ?
