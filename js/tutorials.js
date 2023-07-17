aba85b4ddbf8740063de973dbe604e8a697cda75
hamitmizrak <hamitmizrak@gmail.com>
2023-07-17 20:25:39 +0300

    addEventListener

--- js/tutorials.js
+++ js/tutorials.js
@@ -937,7 +937,32 @@ let domData2 = () => {
 // aşağıda <span id="special_id"></span> içine tıklandı veya paragraf üstüne gelindi.
 
 ////////////////////////////////////////////////////////
+
+// Butona tıkladığımızda kullanıcı soru sorsun
+// Farklı bir sayfaya gitmek ister misiniz ?
+// evet farklı bir sayfaya redirect 
+// değilse alert versin 
+
+let outSite = () => {
+    //alert("deneme");
+    let userInformation =
+        window.confirm("Farklı bir sayfaya gitmek ister misiniz ?")
+            ? window.location="https://www.ecodation.com"
+            : window.alert("Bu sayfada kalındı");
+}
+
+
 // listener
+//  Ödev: Kullanıcı bu paragrafa kaç kere tıkladı ?
+let userListener=()=>{
+    let listenerParag=document.getElementById("parag_list");
+    listenerParag.addEventListener("click",function(e){
+        //alert(e.preventDefault+" "+e.target);
+        document.getElementById("log_admin").innerHTML="takip edildi";
+        //$("#log_admin").html("takip edildi");
+    });
+}
+userListener()
 
 ////////////////////////////////////////////////////////
 // localStorage (Bireysel)
